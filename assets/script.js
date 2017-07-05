6b8567bd7e474a45728aa06a082fedd298451a38
A-Maged <a.maged99@outlook.com>
2017-07-05 04:15:01 +0200

    .

--- assets/script.js
+++ assets/script.js
@@ -8,27 +8,11 @@
 
 
 
-function resizeMsgBox_newline(e) {
+function resizeMsgBox(e){
+	if(e.keyC){
 
-	 if (e.keyCode == 13 && e.shiftKey){
-
-		$(".chat-footer").height(   $(".chat-footer").height()  + 10)  
-
-		$(".msg-box").height(   $(".msg-box").height()  + 10)  
 	}
-
 }
-
-function resizeMsgBox_onpast(e) {
-	var contentHeight = $('.msg-box')[0].scrollHeight
-
-	$(".chat-footer").height(contentHeight + 25)  
-	$(".msg-box").height(contentHeight + 10)  
-
-	console.log(contentHeight)
-}
-
-
 
 // TODO
 	// resizeMsgBox_ondelet()
